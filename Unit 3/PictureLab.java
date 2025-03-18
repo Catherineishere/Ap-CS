62ab9b6ffff26adb6475def1f76d5e19b6a78fde
Catherineishere <chu25@bfacademy.de>
2025-03-18 13:28:06 +0000

    new

---
+++ Unit 3/PictureLab.java
@@ -0,0 +1,9 @@
+public class PictureLab {
+   
+  class Main {
+    public static void main(String[] args) {
+      Picture p = new Picture("beach.jpg");
+      PictureExplorer exp = new PictureExplorer(p);
+    }
+  }
+}
